ed59ef6ddf94f291fa66ecf9e3827e8ec1057e40
tannvq <vaquata206@gmail.com>
2018-11-20 23:39:50 +0700

    page login

--- WebClient/WebClient/Controllers/AccountController.cs
+++ WebClient/WebClient/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebClient.Models;
 
 namespace WebClient.Controllers
 {
@@ -19,6 +20,17 @@ namespace WebClient.Controllers
         public IActionResult Index()
         {
             return this.View();
+        }
+
+        /// <summary>
+        /// Action login
+        /// </summary>
+        /// <param name="login">Login info</param>
+        /// <returns>Redirect to home page</returns>
+        [HttpPost("/login")]
+        public IActionResult Login(LoginViewModel login)
+        {
+            return this.Redirect("/");
         }
     }
 }
---
+++ WebClient/WebClient/Models/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebClient.Models
+{
+    /// <summary>
+    /// Login view model
+    /// </summary>
+    public class LoginViewModel
+    {
+        /// <summary>
+        /// The Username
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// User's password
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Is remember me
+        /// </summary>
+        public bool RememberMe { get; set; }
+    }
+}
